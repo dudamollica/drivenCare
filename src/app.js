38327ba6741fbb442285b7f74c6fc2da1122150b
dudamollica <dudarenno@yahoo.com.br>
2023-04-02 12:57:48 -0300

    Feat: Setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log("servidor funfou"));
